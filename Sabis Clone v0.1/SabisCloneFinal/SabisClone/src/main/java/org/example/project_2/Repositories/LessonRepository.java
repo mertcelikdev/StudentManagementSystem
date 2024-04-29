a91d731b07b60885761f234fabe90563577d1053
Mert Çelik <109079231+mertcelikdev@users.noreply.github.com>
2024-04-29 19:12:22 +0300

    Delete Sabis Clone v0.1/SabisCloneFinal/SabisClone directory

--- Sabis Clone v0.1/SabisCloneFinal/SabisClone/src/main/java/org/example/project_2/Repositories/LessonRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.project_2.Repositories;
-
-import org.example.project_2.Models.Lesson;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LessonRepository extends JpaRepository <Lesson , Long> {
-}
--- Sabis Clone v0.1/SabisCloneFinal/SabisClone/src/main/java/org/example/project_2/Repositories/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.project_2.Repositories;
-
-import org.example.project_2.Models.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student , Long> {
-}
